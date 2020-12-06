df3721317bd699cc740219f49adc9564765373eb
Kirill.Gerasimov <kirill.gerasimov@twino.ru>
2020-12-06 11:45:34 +0300

    Initial commit

---
+++ src/test/kotlin/com/gerkir/rina/RinaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gerkir.rina
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RinaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
